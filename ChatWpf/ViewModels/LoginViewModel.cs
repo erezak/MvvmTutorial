265abc6335c753a1c58fa80c09a13066bec0ff44
Erez A. Korn <erakorn@outlook.com>
2014-09-28 16:19:32 +0300

    Simple Command with CanExecute

--- ChatWpf/ViewModels/LoginViewModel.cs
+++ ChatWpf/ViewModels/LoginViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using YoctoMvvm.Common;
 
 namespace ChatWpf.ViewModels {
@@ -15,11 +16,14 @@ namespace ChatWpf.ViewModels {
             }
             set {
                 SetProperty(ref _Username, value);
+                //Also update the IsLoginEnabled flag, which affects the command CanExecute value
                 if (string.IsNullOrEmpty(value)) {
                     IsLoginEnabled = false;
                 } else {
                     IsLoginEnabled = true;
                 }
+                //Alert the UI systen that the command changed
+                _LoginCommand.RaiseCanExecuteChanged();
             }
         }
 
@@ -35,9 +39,54 @@ namespace ChatWpf.ViewModels {
             }
         }
 
+        private Command _LoginCommand;
+
+        public Command LoginCommand {
+            get {
+                return _LoginCommand;
+            }
+            set {
+                SetProperty(ref _LoginCommand, value);
+            }
+        }
         public LoginViewModel() {
-            _Username = "Test username";
-            _IsLoginEnabled = true;
+            _IsLoginEnabled = false;
+            _LoginCommand = new Command(_LoginAction, () => {
+                return _IsLoginEnabled;
+            });
+        }
+
+        private void _LoginAction() {
+            Username = "Command executed";
+        }
+
+        public class Command : ICommand {
+
+            Func<bool> _CanExecute;
+            Action _ActionToExecute;
+
+            public Command(Action action, Func<bool> canExecute) {
+                _CanExecute = canExecute;
+                _ActionToExecute = action;
+            }
+            public bool CanExecute(object parameter) {
+                return _CanExecute();
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public void RaiseCanExecuteChanged() {
+                if (CanExecuteChanged != null) {
+                    CanExecuteChanged(this, null);
+                }
+            }
+
+            public void Execute(object parameter) {
+                if (_CanExecute()) {
+                    _ActionToExecute();
+                }
+            }
+
         }
     }
 }

8c9f74f63931f00d0b4a0184e5f230024ef8e04e
Erez A. Korn <erakorn@outlook.com>
2014-09-28 15:48:06 +0300

    Simple View Model with two way binding

---
+++ ChatWpf/ViewModels/LoginViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoctoMvvm.Common;
+
+namespace ChatWpf.ViewModels {
+    class LoginViewModel : Bindable {
+        private String _Username;
+
+        public String Username {
+            get {
+                return _Username;
+            }
+            set {
+                SetProperty(ref _Username, value);
+                if (string.IsNullOrEmpty(value)) {
+                    IsLoginEnabled = false;
+                } else {
+                    IsLoginEnabled = true;
+                }
+            }
+        }
+
+
+        private bool _IsLoginEnabled;
+
+        public bool IsLoginEnabled {
+            get {
+                return _IsLoginEnabled;
+            }
+            set {
+                SetProperty(ref _IsLoginEnabled, value);
+            }
+        }
+
+        public LoginViewModel() {
+            _Username = "Test username";
+            _IsLoginEnabled = true;
+        }
+    }
+}
